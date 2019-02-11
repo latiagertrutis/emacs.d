b4d725d7add7b5ce9da8f6b5155ee4e0b4aada6e
unchartech_2 <mateo.rodriguez@unchartech.com>
2019-02-11 17:49:32 +0100

    indent buffer

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,4 +1,24 @@
 (global-hl-line-mode +1)
 (setq-default cursor-type 'bar)
 
+;; indent all the buffer
+(defun indent-buffer ()
+  "Just indent all the buffer."
+  (interactive)
+  (save-excursion (indent-region (point-min) (point-max) nil)))
+(global-set-key (kbd "C-c i") 'indent-buffer)
+
+;; revert all the buffers
+(defun revert-all-buffers ()
+  "Revert all non-modified buffers associated with a file.
+This is to update existing buffers after a Git pull of their underlying files."
+  (interactive)
+  (save-current-buffer
+    (mapc (lambda (b)
+            (set-buffer b)
+            (unless (or (null (buffer-file-name)) (buffer-modified-p))
+              (revert-buffer t t)
+              (message "Reverted %s\n" (buffer-file-name))))
+          (buffer-list))))
+
 (provide 'init-local)
