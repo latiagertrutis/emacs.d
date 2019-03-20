1a3a08746b25697cbeb690d2f52b0455bb07b718
Mateo <mateo.rodriguez@unchartech.com>
2019-03-20 09:43:57 +0100

    font on all frames

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -37,6 +37,6 @@ This is to update existing buffers after a Git pull of their underlying files."
      (other-frame -1))))
 
 ;;default font
-(set-default-font "Monospace Serif")
+(set-default-font "Monospace Serif" nil t)
 
 (provide 'init-local)
