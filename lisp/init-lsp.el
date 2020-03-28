1d762581174e6d39c8e77cf4a7d794423cfeb877
Mateo Rodriguez <teorodrip@posteo.net>
2020-03-28 21:42:39 +0100

    company lsp backends

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,6 +1,10 @@
 ;; Requires gopls installatio to work:
 ;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
 (require-package 'lsp-mode)
-(require-package 'company-lsp)
-(push 'company-lsp company-backends)
+
+
+(with-eval-after-load 'company
+  (require-package 'company-lsp)
+  (push 'company-lsp company-backends))
+
 (provide 'init-lsp)
