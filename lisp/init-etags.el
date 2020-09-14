04086625646365fc3ad6a30ba240b381e6c66b8a
latiagertrutis <30806947+latiagertrutis@users.noreply.github.com>
2020-09-14 14:05:15 +0200

    etags upgrade

--- lisp/init-etags.el
+++ lisp/init-etags.el
@@ -1,23 +1,34 @@
-(require-package 'etags)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Filename: init-etags.el
+;;Description:
+;;Author: Mateo Rodriguez Ripolles (teorodrip@posteo.net)
+;;Maintainer:
+;;Created: Thu Sep 10 15:38:07 2020 (+0200)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq tags-file-name "~/.emacs.d/TAGS")
-(setq tag-file ".TAGS")
+(use-package etags
+             :ensure t
+             :bind
+             (("M-g d" . xref-find-definitions)
+              ("M-g r" . xref-find-references)
+              ("M-g b" . xref-pop-marker-stack)
+              ("M-g o" . xref-find-definitions-other-window)))
+
+(setq etags-file "TAGS")
 
 (defun refresh-project-tags ()
   "Rewfres the TAG file of one project."
   (interactive)
-  (let
-      ((tag-dir (locate-dominating-file buffer-file-name tag-file)))
+  (let*
+      ((tag-dir (locate-dominating-file buffer-file-name etags-file))
+       (relative-etags-file (concat (file-name-as-directory tag-dir) etags-file)))
+    (print (format "Tag dir is: %s" relative-etags-file))
+    (setq tags-table-list `(,tag-dir))
     (if tag-dir
         (shell-command (format
                         "find %s \\( -name \"*.[chCH]\" -o -name \"*.go\" \\) -print | etags - -o %s"
                         tag-dir
-                        "~/.emacs.d/TAGS"))
-      (print (format "No %s file found in project." tag-file)))))
-
-(bind-key* "M-g d" 'xref-find-definitions)
-(bind-key* "M-g r" 'xref-find-references)
-(bind-key* "M-g b" 'xref-pop-marker-stack)
-(bind-key* "M-g o" 'xref-find-definitions-other-window)
+                        relative-etags-file))
+      (print (format "No %s file found in project." etags-file)))))
 
 (provide 'init-etags)
